f30b3d24f267090f170e89eb43b478760f7c0efb
markbus-ai <58405544+markbus-ai@users.noreply.github.com>
2024-04-22 07:56:32 -0300

    Delete backend directory

--- backend/type_hints.py
+++
@@ -1,6 +0,0 @@
-### type hints ###
-
-#definiendo el tipo de mi variable como string
-#sirve para que el autocompletado sea orientado a ese tipo
-#y asi fast api sabe lo que tiene que esperar del usuario
-mi_variable_string: str = "hola mundo"
